05c23a163eea01f0386a98c2c442cae794dda437
Roberto Blasco~ <roberza2202@gmail.com>
2025-07-14 01:38:42 +0200

    servidor, cors, dotenv, express json y urlencoded

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const PORT = process.env.PORT || 3001;
+
+dotenv.config();
+
+app.use(cors({
+  origin: 'http://localhost:5173', // o el puerto donde corre tu vite frontend
+  credentials: true // Configurar así cors para que reciba credenciales desde el front
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
