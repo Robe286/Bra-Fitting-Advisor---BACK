29b7e7fefcfc7cbec95336874e6417f4ae38b2e4
Roberto Blasco~ <roberza2202@gmail.com>
2025-07-27 20:36:44 +0200

    solventar error de CORS

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const dbConection = require('./config/db.js')
 const productRoutes = require('./routes/productRoutes.js');
 const sizeRoutes = require('./routes/sizeRoutes.js');
 const authRoutes = require('./routes/authRoutes.js');
-const favoriteRoutes = require('./routes/favoriteRoutes.js');
+//const favoriteRoutes = require('./routes/favoriteRoutes.js');
 
 dotenv.config();
 
 app.use(cors({
-  origin: 'http://localhost:5173', // o el puerto donde corre tu vite frontend
+  origin: 'https://brafittingadvisor.netlify.app/',
   credentials: true // Permite que se reciban credenciales desde el front
 }));
 app.use(express.json());
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api/auth', authRoutes);
 app.use('/api/sizes', sizeRoutes);
 app.use('/api/products', productRoutes);
-app.use('api/favorites', favoriteRoutes); // Implementar a futuro
+//app.use('api/favorites', favoriteRoutes); // Implementar a futuro
 
 dbConection()
 
